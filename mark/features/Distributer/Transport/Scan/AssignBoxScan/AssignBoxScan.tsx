fc513ca106e3d45ca23c7aa7e559e8e2b2525be6
Piyakorn Sutreerojthakool <Piyakorn2112@hotmail.com>
2025-06-24 14:59:57 +0700

    qweweeqeqe

--- mark/features/Distributer/Transport/Scan/AssignBoxScan/AssignBoxScan.tsx
+++ mark/features/Distributer/Transport/Scan/AssignBoxScan/AssignBoxScan.tsx
@@ -1,13 +1,65 @@
-import React from 'react'
+
+'use client';
+import style from './AssignBoxScan.module.css'
+import { useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import LogoRole from "@/components/logoRole/LogoRole";
+import LogoutButton from "@/components/logoutButton/LogoutButton";
+import QrScanner, { QrScannerHandle } from '@/components/Scanner/Scanner';
+import { Button } from '@mantine/core';
+import assignToShipping from '@/api/lot/assignToShipping';
+import { useAuth } from "@/providers/AuthContext";
 
 type AssignProps = {
   shippingId: string;
 }
 
 const AssignBoxScan = ({ shippingId }: AssignProps) => {
+   const router = useRouter();
+  const { user } = useAuth();
+  const scannerRef = useRef<QrScannerHandle>(null);
+  const [id, setId] = useState('');
+
+  const handleScan = async (id: string) => {
+    console.log('Scanned ID:', id);
+    setId(id);
+  };
+
+  const handleContinue = async() =>{
+    await scannerRef.current?.stopScanner();
+    const res = await assignToShipping(shippingId, id, user.token);
+    console.log(res)
+    router.push(`/distributer/transport/shippinginfo/${encodeURIComponent(shippingId)}`);
+  }
+
+  const handleBack = () => {
+    router.push('/distributer/home')
+  }
+ 
   return (
-    <div>AssignBoxScan</div>
-  )
+    <div className={style.Backdrop}>
+         <img className={style.BackdropArt} src="/images/PathBackDrop.svg" />
+      <div className={style.BackdropShade} />
+      <div className={style.LogoContainer}>
+        <LogoRole text={"distributer"} />
+      </div>
+      <div className={style.LogoutButtonContainer}>
+        <LogoutButton />
+      </div>
+      <div className={style.ContainerCard}>
+        <QrScanner ref={scannerRef} onScan={handleScan} />
+        { id !== '' &&
+      <div className={style.ActionContainer}>
+        <Button variant='green-md' onClick={handleContinue}>เพิ่มกล่อง</Button>
+      </div>
+      }
+      <div className={style.BackButtonContainer}>
+        <Button onClick={handleBack}>{'<-'} ย้อนกลับ</Button>
+      </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default AssignBoxScan
