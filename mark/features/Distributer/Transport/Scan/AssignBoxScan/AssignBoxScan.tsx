bc1ca3ad81e448cc76781b57b2aa16acf5571cd9
Piyakorn Sutreerojthakool <Piyakorn2112@hotmail.com>
2025-06-24 14:33:17 +0700

    kjrfdlfkjfkdf

---
+++ mark/features/Distributer/Transport/Scan/AssignBoxScan/AssignBoxScan.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type AssignProps = {
+  shippingId: string;
+}
+
+const AssignBoxScan = ({ shippingId }: AssignProps) => {
+  return (
+    <div>AssignBoxScan</div>
+  )
+}
+
+export default AssignBoxScan
